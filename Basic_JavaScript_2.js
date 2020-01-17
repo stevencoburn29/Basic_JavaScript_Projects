8bd2b298647420dd108d28826f0afb06c099567e
stevencoburn29 <43035325+stevencoburn29@users.noreply.github.com>
2020-01-17 13:46:18 -0800

    Add files via upload

---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	var sentence = "PUSH IT REAL GOOD!";
+	sentence += " Actually, no. I'm ashamed of you for pushing too hard.";
+	document.getElementById("Push it").innerHTML = sentence;
+}
