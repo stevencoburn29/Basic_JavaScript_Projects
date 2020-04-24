a7d1250676df766474c32e1a6bbf3197df43bf49
stevencoburn29 <43035325+stevencoburn29@users.noreply.github.com>
2020-04-24 08:25:52 -0700

    Add files via upload

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+x = "My boy ";
+y = " and my girl.";
+var x = x.fontcolor("blue");
+var y = y.fontcolor("red");
+
+
+window.alert("I'm alerting you for no particular reason.");
+
+document.write(x+y);
+
+
+function Functional_Function() {
+    var z = "I'm purple";
+    var result = z.fontcolor("purple");
+    document.getElementById("Identify_Me").innerHTML = result;
+}
